42ef113eba2c440e622bc125fa11a9592dde283f
Hamza Naseer <naseer1hamza@gmail.com>
2021-02-22 20:07:30 -0800

    menu item done

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./homepage.styles.scss";
+import Directory from '../../component/directory/directory.component.jsx'
+
+const HomePage = () => (
+    <div className='homepage'>
+      <h1>Welcome to my Homepage</h1>
+      <Directory />
+    </div>
+  );
+
+export default HomePage;
